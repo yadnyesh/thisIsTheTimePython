7fc06ed7ffb8b9ea6ff7e8efc83935cf522e6dfe
yadnyesh <yadnyesh.juvekar@gmail.com>
2023-09-10 11:18:59 +0530

    dictionary

--- cs50/hogwarts.py
+++ cs50/hogwarts.py
@@ -1,9 +1,39 @@
-students  = {
+student  = {
+
+
     "Hermione": "Gryffindor",
     "Harry": "Gryffindor",
     "Ron": "Gryffindor",
     "Draco": "Slytherin"
 }
 
+students = [
+    {
+        "name": "Hermione",
+        "house": "Gryffindor",
+        "patronous": "Otter"
+    },
+        {
+        "name": "Harry",
+        "house": "Gryffindor",
+        "patronous": "Stag"
+    },
+    {
+        "name": "Ron",
+        "house": "Gryffindor",
+        "patronous": "Jack Russell terrier"
+    },
+    {
+        "name": "Hermione",
+        "house": "Gryffindor",
+        "patronous": "Otter"
+    },
+    {
+        "name": "Draco",
+        "house": "Slytherin",
+        "patronous": None
+    }
+]
+
 for student in students:
-    print(student, students[student], sep=",")
+    print(student["name"], student["house"], sep=", ")
